cdc30819b6c08af5c7aafe3c3e4603c41267ed91
ESTUDIANTES <ESTUDIANTES@puce.edu.ec>
2026-05-21 15:48:19 -0500

    practica

---
+++ src/app/services/usuario-service.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Usuario } from '../models/usuario';
+import { map, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UsuarioService {
+
+  private http = inject(HttpClient);
+
+  private API_USARIOS = 'https://veterinariaaws-default-rtdb.firebaseio.com/usuarios.json'
+
+  //Método POST
+  postUsuarios(usuario:Usuario):Observable<Usuario>{
+    return this.http.post<Usuario>(this.API_USARIOS, usuario);
+  }
+  
+  //GET
+ getUsuarios(): Observable<Usuario[]> {
+  return this.http
+    .get<{ [key: string]: Usuario }>(this.API_USARIOS)
+    .pipe(
+      map(res => {
+        if (!res) return [];
+
+        return Object.keys(res).map(id => ({
+          ...res[id],
+          id: id
+        }));
+      })
+    );
+}
+}
